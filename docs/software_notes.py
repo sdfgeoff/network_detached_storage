2e1418f1280504f31655d1f52a169f7ee79d34eb
Geoffrey Irons <sdfgeoff@gmail.com>
2022-08-27 08:32:32 +1200

    Enlarge suspension plate to add clearange for bolts

---
+++ docs/software_notes.py
@@ -0,0 +1,6 @@
+1) Host a wifi network
+2) Redirect to a landing page
+3) Landing page is custom page allowing users to create/sign in to an account
+4) An account gives permission to upload files. 
+    - Files are public to other users.
+    - Files are a drop-out cache. When disk approaches 100% full, old files are deleted
